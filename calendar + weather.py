308436e3ee65ec87dc476c4034555f522c2e1058
denisalis666@gmail.com <ujkjdfxtd382>
2020-06-12 15:35:06 +0300

    calendar + weather.py - added a weather display script, you need to understand how to display the weather display on the form (добавил скрипт отображения погоды, нужно понять как вывести отображение погоды на форму).

--- calendar + weather.py
+++ calendar + weather.py
@@ -4,23 +4,24 @@ from tkinter import *
 import requests
 from tkcalendar import *
 
-# возможно тоже здесь лишнее
-
-window = Tk()
-window.geometry('400x250')
-window.title('Calendar + weather')
+root = Tk()
+root.geometry('400x250')
+root.title('Calendar + weather')
 
 
-def get_weath():
-    req = requests.get('http://wttr.in/?0T')
-    print(req.text)
+def tell_weather():
+    url = 'http://wttr.in/?0T'
+    response = requests.get(url)
+    x = response.json()
 
 
 # define the window's appearance (определим место появления окна)
-f2 = Frame(window, width=2000, height=2000, relief=SUNKEN, bd=4, bg='white')
+f1 = Button(root,text='Weather', foreground='black')
+f1.grid(row=0, column=1)
+f2 = Frame(root, relief=SUNKEN, bd=4, bg='white')
 f2.grid()
-f3 = Label(window, width=40, height=5, relief=SUNKEN, bd=4, bg='white')
-f3.grid(row=0, column=1)
+f3 = Label(root, width=40, height=5, relief=SUNKEN, bd=4, bg='white')
+f3.grid(row=1, column=1)
 
 # определим оформление календаря
 cal = DateEntry(f2, dateformat=3, width=12, background='white', foreground='black', borderwidth=4, Calendar=2020)
@@ -31,4 +32,4 @@ url = 'http://wttr.in/?0T'
 response = requests.get(url)  # выполните HTTP-запрос
 print(response.text)  # напечатайте текст HTTP-ответа
 
-window.mainloop()
+root.mainloop()
