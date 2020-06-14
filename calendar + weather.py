a822fcf1fd544b9d669167587b98da63097bea07
denisalis666@gmail.com <ujkjdfxtd382>
2020-06-14 12:20:16 +0300

    calendar + weather.py - added a weather display script, you need to understand how to display the weather display on the form (добавил скрипт отображения погоды, нужно понять как вывести отображение погоды на форму).

--- calendar + weather.py
+++ calendar + weather.py
@@ -12,19 +12,22 @@ root.title('Calendar + weather')
 def tell_weather():
     url = 'http://wttr.in/?0T'
     response = requests.get(url)
-    x = response.json()
+    print(response.text)
 
 
 # define the window's appearance (определим место появления окна)
-f1 = Button(root,text='Weather', foreground='black')
-f1.grid(row=0, column=1)
-f2 = Frame(root, relief=SUNKEN, bd=4, bg='white')
-f2.grid()
-f3 = Label(root, width=40, height=5, relief=SUNKEN, bd=4, bg='white')
-f3.grid(row=1, column=1)
+btn_Weather = Button(root, text='Weather', foreground='black', command=tell_weather)
+btn_Weather.bind("<Button-1>", tell_weather)
+btn_Weather.grid(row=0, column=1)
+
+date_ent = Frame(root, relief=SUNKEN, bd=4, bg='white')
+date_ent.grid()
+
+weather_output = Label(root, text="Нажмите 'Weather' чтобы получить прогноз погоды")
+weather_output.grid(row=1, column=1)
 
 # определим оформление календаря
-cal = DateEntry(f2, dateformat=3, width=12, background='white', foreground='black', borderwidth=4, Calendar=2020)
+cal = DateEntry(date_ent, dateformat=3, width=12, background='white', foreground='black', borderwidth=4, Calendar=2020)
 cal.grid(row=1, column=3, sticky='nsew')
 
 # define the weather (определим погоду)
